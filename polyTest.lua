b37cd5119b726b87cb75420e4accf146b7e7d065
SleepingFox8 <SleepingFox8@gmail.com>
2021-04-23 00:58:50 -0400

    Get zones from internet

--- polyTest.lua
+++ polyTest.lua
@@ -43,6 +43,63 @@
         return oddNodes 
     end
 
+    --gets a file from a url and puts it in a table
+    local function getFile(url, timeout)
+        local settings={}
+        if not timeout then
+            settings.httpTimeout=10000
+        else
+            settings.httpTimeout=timeout
+        end
+        local http=httpRequest({url=url}, {url=url, requestMethod="GET", timeout=settings.httpTimeout})
+        local file=http["input"]
+        local err=http.getResponseCode()
+        local line=file:readLine()
+        local output={}
+        while line~=nil do
+            table.insert(output, line)
+            line=file:readLine()
+        end
+        return output
+    end
+
+    local function getFileStringFromURL(url)
+        local fileResults = getFile(url, 10000)
+        local fileString = ""
+        for key,value in pairs(fileResults) do 
+            fileString = fileString..value.."\n"
+        end
+        return fileString
+    end
+
+    local function formatGitZonesToUseable(gitZone)
+        --function initialization
+            --initialize function table
+                local FUNC = {}
+            --store arguments in known scoped table
+                FUNC.gitZone = gitZone
+
+        FUNC.useableZones = {}
+
+        for key,value in pairs(FUNC.gitZone["features"]) do
+            FUNC.i = key
+
+            FUNC.name = FUNC.gitZone["features"][FUNC.i]["name"]
+
+            -- log zones with multiple polygons
+                -- if #FUNC.gitZone["features"][FUNC.i]["polygon"] ~= 1 then
+                --     slog(FUNC.name)
+                --     slog(#FUNC.gitZone["features"][FUNC.i]["polygon"])
+                -- end
+            
+            FUNC.polygon = FUNC.gitZone["features"][FUNC.i]["polygon"][1]
+
+            FUNC.useableZones[FUNC.name] = FUNC.polygon
+        end
+
+        return FUNC.useableZones
+    end
+
 -- Main program
 
     --initialize MAIN table
@@ -53,11 +110,17 @@
     slog("Started displaying current zones")
 
     -- get zone data from file
-        MAIN.zoneJson = json.decode(compTools.readAll("./Public-CivClassic-Nodes/zones.json"))
+        -- MAIN.zoneJson = json.decode(compTools.readAll("./Public-CivClassic-Nodes/zones.json"))
 
-        -- for key,value in pairs(MAIN.zoneJson) do
-        --     log(key)
-        -- end
+    -- get zone data from GitHub
+
+        slog("downloading zones")
+        MAIN.fileString = getFileStringFromURL("https://raw.githubusercontent.com/SleepingFox8/data/master/land_claims.civmap.json")
+
+        slog("parsing git zones")
+        MAIN.zoneJson = formatGitZonesToUseable(json.decode(MAIN.fileString))
+        slog("done parsing git")
+        
 
     -- initialize GUI table
         GLBL.GUI = GLBL.GUI or {}
@@ -82,13 +145,16 @@
                     MAIN.isInsideZone = insidePolygon(MAIN.polygon, MAIN.point)
 
                     if MAIN.isInsideZone then
+
+                        -- replace "\n"s in name with " "
+                        MAIN.zoneName = string.gsub(MAIN.zoneName, "\n", " ")
                         MAIN.insideZones[#MAIN.insideZones+1] = MAIN.zoneName
                     end
                 end
 
                 -- MAIN.insideZones = compTools.sortTableByKeys(MAIN.insideZones)
 
-                -- log(MAIN.insideZones)
+                -- slog(MAIN.insideZones)
 
                 -- turn answers into string
                     if #MAIN.insideZones > 0 then
@@ -109,5 +175,6 @@
                 GLBL.GUI.zone = hud2D.newText("Current location: " .. MAIN.outputGuiString, 5, MAIN.drawn)
                 GLBL.GUI.zone.enableDraw()
         sleep(100)
+        -- slog("rendered")
     end
     
